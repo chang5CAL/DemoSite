75f8236604091a1a712e0f1af6b480b5eda76e4b
chang5CAL <chang5@calcharter.org>
2016-08-22 13:04:43 -0700

    Added admin pages

--- demo/js/load.js
+++ demo/js/load.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
 	$('.nav-container').load('/templates/nav.html')
 });
 
+$(document).ready(function() {
+	$('.nav-container-admin').load('/templates/adminNav.html')
+});
+
+$(document).ready(function() {
+	$('.nav-container-admin-logout').load('/templates/adminNavLogout.html')
+});
+
 // Initialize Firebase
 
 firebase.initializeApp(config);
