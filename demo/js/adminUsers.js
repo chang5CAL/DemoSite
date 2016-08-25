75d1094c4153a981f3154c0f994569f524adfd6d
chang5CAL <chang5@calcharter.org>
2016-08-25 16:49:02 -0700

    Added mass uploading for users

--- demo/js/adminUsers.js
+++ demo/js/adminUsers.js
@@ -4,11 +4,15 @@ var companies = {};
 var companyList = [];
 var reverseCompanyMapping = {};
 var reverseDeptMapping = {};
+var reverseMassInput = {};
+
 
 var departments = {};
 var departmentList = [];
 
 var secondaryApp = firebase.initializeApp(config, "Secondary");
+
+var data = [];
 
 var toDoubleDigit = function(date) {
 	if (date < 10) {
@@ -17,6 +21,65 @@ var toDoubleDigit = function(date) {
 		return date;
 	}
 }
+
+var upload = function() {
+	console.log("Running Upload");
+  	var file = $("#file")[0].files[0];
+	var company = $('#company').val();
+/*	if (company == "") {
+		alert("Please enter the company name to add csv file to");
+		$('#file').val('');
+		return false;
+	}*/
+	console.log("Company present");
+	if (file && file.name.substring(file.name.length - 4) == ".csv") {
+		console.log("Is valid file");
+		Papa.parse(file, {
+			complete: function(results) {
+				data = results.data;
+				console.log(data);
+				massCreate();
+			}
+		})
+	}
+}
+
+var massCreate = function(){firebase.auth().onAuthStateChanged(function(user) {
+  // redirect if the user is signed in
+  var emailToIdMapping = {};
+  for(var i = 0; i < data.length; i++) {
+  	emailToIdMapping[data[i][0]] = i;
+  	if(data[i].length == 6){
+			secondaryApp.auth().createUserWithEmailAndPassword(data[i][0],data[i][1]).then(function(firebaseUser) {
+		    var index = emailToIdMapping[firebaseUser.email];
+
+		    console.log("User " + firebaseUser.uid + " created successfully!");
+				console.log(i);
+				date = new Date();
+				time = date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+				created = toDoubleDigit(date.getMonth() + 1) + "/" + toDoubleDigit(date.getDate()) + "/" + date.getFullYear() + " " + time;
+			    obj = {
+						companyId: reverseCompanyMapping[data[index][4]],
+			    	companyName: data[index][4],
+						createdAt: created,
+						departmentId: reverseDeptMapping[data[index][5]],
+						departmentName: data[index][5],
+			    	email: data[index][0],
+			    	title: data[index][2],
+			    	userName: data[index][3]
+			    }
+
+				firebase.database().ref("/Users/" + firebaseUser.uid).set(obj).catch(function(error) {
+					console.log(error.message)
+				});
+			    //I don't know if the next statement is necessary 
+			    secondaryApp.auth().signOut();
+	  		})
+			}
+		}
+		})
+	}
+
 
 var deptComplete = function(ui) { 
 	if (typeof departments[reverseCompanyMapping[ui]] !== 'undefined'){
@@ -44,7 +107,7 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
   	console.log(user.uid);
   	$("#signup-btn").click(function(){
-		secondaryApp.auth().createUserWithEmailAndPassword("testvalemail12@gmail.com", "pwd123").then(function(firebaseUser) {
+		secondaryApp.auth().createUserWithEmailAndPassword($("#email").val(), $("#password").val()).then(function(firebaseUser) {
 		    console.log("User " + firebaseUser.uid + " created successfully!");
 			date = new Date();
 			time = date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
@@ -69,7 +132,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 		    	companyName: "TestCompany",
 		    	email: "TestEmail"
 		    }*/
-			firebase.database().ref("/testUsers/" + firebaseUser.uid).set(obj).catch(function(error) {
+			firebase.database().ref("/Users/" + firebaseUser.uid).set(obj).catch(function(error) {
 				console.log(error.message)
 			});
 		    //I don't know if the next statement is necessary 
@@ -94,7 +157,6 @@ ref.once('value', function(snapshot) {
 		companyList.push(companies[key].companyName);
 		reverseCompanyMapping[companies[key].companyName] = key;
 	}
-	console.log(companyList);
 })
 
 deptRef.once('value', function(snapshot) {
@@ -116,4 +178,17 @@ $($("#company").change(function() {
 	console.log("Running");
 	deptComplete($("#company").val());
 	
-}));
+}))
+
+$(document).ready(function(){
+	$('#file').change(function (e) {
+		console.log("File changed");
+	  	var file = this.files[0];
+	  	if (!file && file.name.substring(file.name.length - 4) != ".csv") {
+	  		alert("Please upload a valid csv file");
+	  	} else {
+	  		upload();
+	  		//uploaded = true;
+	  	}
+	})
+});
