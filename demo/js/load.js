54548b9ee8b26c1478728b0ca04d048b105351b6
chang5CAL <ginka13@gmail.com>
2016-08-26 14:44:49 -0700

    fixed upper casing

--- demo/js/load.js
+++ demo/js/load.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-	$('.nav-container-logout').load('/templates/NavLogout.html')
+	$('.nav-container-logout').load('/templates/navLogout.html')
 });
 
 
